c42f68da47a571a023bc29edcf5fa9ada1febf91
earlroy <eorlroy@126.com>
2018-11-20 22:25:17 +0800

    更新代码

--- AssistTool/Form1.cs
+++ AssistTool/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Windows.Forms;
 
@@ -6,7 +7,13 @@ namespace AssistTool
 {
     public partial class IndexForm : Form
     {
-        Dictionary<string, Model.AccountConfig> dic = new Dictionary<string, Model.AccountConfig>();
+        private const string BUTTON_ACTION_TXT = "开始";
+        private const string BUTTON_STOP_TXT = "停止";
+        private const string BUTTON_WAIT_TXT = "等待";
+        private static Model.ThreadConfig busThreadConfig = new Model.ThreadConfig();//总线任务
+        private static List<Model.ThreadConfig> threadConfigList = new List<Model.ThreadConfig>();//不需要线程安全的
+        Dictionary<string, Model.AccountConfig> accountConfigDic = new Dictionary<string, Model.AccountConfig>();
+
         public IndexForm()
         {
             InitializeComponent();
@@ -25,19 +32,22 @@ namespace AssistTool
                     List<Model.AccountConfig> acountConfigList = JsonHelper.DeserializeJsonToObject<List<Model.AccountConfig>>(configTxt);
                     if (acountConfigList != null)
                     {
+                        string actionId = null;
                         IOHelper.CreateDir(Common.DATA_PATH);//内部有存在性判断
                         foreach (Model.AccountConfig config in acountConfigList)
                         {
+                            actionId = Utils.MD5(config.Email.Trim());
                             if (!this.CheckAccountConfig(config))
                             {//如果信息有误,跳过
                                 continue;
                             }
-                            if (!dic.ContainsKey(Utils.MD5(config.Email.Trim())))
+                            if (!accountConfigDic.ContainsKey(actionId))
                             {//防止重复代码
-                                dic.Add(Utils.MD5(config.Email.Trim()), config);
+
+                                accountConfigDic.Add(actionId, config);
                             }
                         }
-                        if (dic.Count <= 0)
+                        if (accountConfigDic.Count <= 0)
                         {
                             this.ShowAndClose("未检测到有效账户数据!");
                         }
@@ -53,7 +63,7 @@ namespace AssistTool
                 }
                 #endregion
 
-                foreach (var item in dic)
+                foreach (var item in accountConfigDic)
                 {
                     AccountComboBox.Items.Add(item.Value.Email);
                 }
@@ -63,11 +73,22 @@ namespace AssistTool
             {
                 this.ShowAndClose(ex.ToString());
             }
+            ActionButton.Text = BUTTON_ACTION_TXT;
         }
         #region Event
         private void button1_Click(object sender, EventArgs e)
         {
-            ActionButton.Text = string.Equals(ActionButton.Text, "开始") ? "停止":"开始";
+            if (string.Equals(ActionButton.Text, BUTTON_ACTION_TXT))
+            {//执行任务
+                
+                ActionButton.Text = BUTTON_STOP_TXT;
+            }
+            else
+            {//执行停止任务
+                ActionButton.Text = BUTTON_WAIT_TXT;
+
+                ActionButton.Text = BUTTON_ACTION_TXT;
+            }
         }
 
         private void Form_Closing(object sender, FormClosingEventArgs e)
@@ -111,6 +132,18 @@ namespace AssistTool
                 return false;
             }
             return true;
+        }
+        /// <summary>
+        /// 添加任务
+        /// </summary>
+        private static void AddTaskConfig(Model.ThreadConfig config)
+        {
+            if (config != null 
+                && !string.IsNullOrWhiteSpace(config.ID) 
+                && !threadConfigList.Exists(item=>string.Equals(item.ID, config.ID)))
+            {//如果config有效且集合无重复值,添加集合&添加总线
+                
+            }
         }
         /// <summary>
         /// 展示信息并关闭程序
---
+++ AssistTool/Model/ThreadConfig.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+
+namespace AssistTool.Model
+{
+    public class ThreadConfig
+    {
+        /// <summary>
+        /// ID
+        /// </summary>
+        public string ID { get; set; }
+        /// <summary>
+        /// 时间
+        /// </summary>
+        public int DateTime { get; set; }
+        /// <summary>
+        /// 总任务数
+        /// </summary>
+        public int CountTaskNum { get; set; }
+        /// <summary>
+        /// 当前任务数
+        /// </summary>
+        public int FinishTaskNum { get; set; }
+        /// <summary>
+        /// 是否空闲
+        /// </summary>
+        public bool Idle { get; set; }
+        /// <summary>
+        /// 消息池
+        /// </summary>
+        public Queue Message { get; set; }
+    }
+}
